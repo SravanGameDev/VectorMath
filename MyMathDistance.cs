6544bc08f82904671e1947c8bce75d137b2c2454
Sravan kumar <sravan.kumar1691@gmail.com>
2020-12-17 14:24:03 +0530

    Added Distance, Magnitude, Normalize a Vector

---
+++ MyMathDistance.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMathDistance : MonoBehaviour
+{
+
+    public Vector2 currentPosition;
+    public float xPos;
+    public  float yPos;
+    public float total;
+    public float distance;
+    public float normalizeXPos;
+    public float normalizeYPos;
+    public Vector2 normalization;
+
+    void LengthOfVector()
+    {
+        currentPosition = new Vector2(transform.position.x, transform.position.y);
+
+        xPos = (transform.position.x * transform.position.x);
+        yPos = (transform.position.y * transform.position.y);
+
+        total = xPos + yPos;
+
+        distance =(float) System.Math.Sqrt(total);
+
+        normalizeXPos = transform.position.x / distance;
+        normalizeYPos = transform.position.y / distance;
+
+        normalization = new Vector2(normalizeXPos, normalizeYPos);
+        #region Debugs
+        /*
+        Debug.Log($"Length of a vector is {distance}");
+
+        Debug.Log($"Magnitude of a Vector is {pt.magnitude} ");
+
+        Debug.Log($"Distance of a Vector is {Vector3.Distance(Vector3.zero,pt)} ");
+        */
+        #endregion
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        LengthOfVector();
+
+        Gizmos.DrawLine(Vector2.zero, normalization);
+
+    }
+}
+
+
---
+++ RadialTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadialTrigger : MonoBehaviour
+{
+    [Range(0, 5)]
+    public float radius = 1;
+
+    public Transform obj;
+
+    private void OnDrawGizmos()
+    {
+        Vector3 origin = transform.position;
+        Vector3 enemy = obj.position;
+
+        Vector3 distance = enemy - origin;
+
+        float length = distance.x * distance.x + distance.y * distance.y;
+        //length = Mathf.Sqrt(length);
+
+        //Squaring on both side will element the square root for length
+        //This is one of the optimization technique
+
+        bool inInside = length < radius*radius;
+
+        Gizmos.color=inInside ?  Color.green : Color.red;
+
+        Gizmos.DrawWireSphere(origin, radius);
+
+    }
+}
